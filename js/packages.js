ae25af310baff0642268247072785d750cdcc464
Rachel Lu <lurachel18@gmail.com>
2020-06-09 17:40:20 -0400

    Implement package search

--- js/packages.js
+++ js/packages.js
@@ -1,3 +1,16 @@
+let allPackages;
+
+$.getJSON('./output.json',  function(responseObject){
+    allPackages = responseObject.source;
+    initializePackages(allPackages);
+    console.log(allPackages)
+});
+
+var initializePackages = function(packagesList){
+    renderPackages(packagesList);
+    let allPackages = packagesList;
+}
+
 var renderPackages = function(packagesList, searchString="") {
     // Parent div to hold all the package cards
     var mainDiv = document.getElementsByClassName("package-results")[0];
@@ -35,27 +48,54 @@ var renderPackages = function(packagesList, searchString="") {
                 packageDiv.appendChild(websiteLink)
             }
 
-
-            // // Metrics button
-            // var metricsButton = document.createElement('button')
-            // metricsButton.setAttribute("onclick", "window.open('https://opensource.twitter.com/metrics/" + package.nameWithOwner + "/WEEKLY')")
-            // metricsButton.type = "button"
-            // metricsButton.className = "Button Button--tertiary"
-            // metricsButton.innerHTML = "Metrics"
-            // packageDiv.appendChild(metricsButton)
-
-            /* Finally Add the package card to the page */
+            // Add the package card to the page
             mainDiv.appendChild(packageDiv)
         }
     } else {
         var noResultDiv = document.createElement('div')
-        noResultDiv.className = 'no-results'
+        noResultDiv.className = 'package-card'
 
         var noResultPara = document.createElement('p')
         noResultPara.innerHTML = "No results for " + '<b>' + searchString + '</b>'
         noResultDiv.appendChild(noResultPara)
 
-        var noResultContainer = document.getElementsByClassName("no-results-container")[0]
-        noResultContainer.appendChild(noResultDiv)
+        mainDiv.appendChild(noResultDiv)
+    }
+}
+
+function searchPackages() {
+    var query = document.getElementsByClassName("search-box")[0].value.trim();
+    console.log(query)
+    clearPackages();
+    if (query === '') {
+        renderPackages(allPackages);
+    } 
+    else {
+        var options = {
+          findAllMatches: true,
+          threshold: 0.2,
+          location: 0,
+          distance: 50,
+          maxPatternLength: 50,
+          minMatchCharLength: 1,
+          keys: [
+            "Name",
+            "Description"
+          ]
+        }
+        var fuse = new Fuse(allPackages, options);
+        var searchResult = fuse.search(query);
+        var newPackagesList = [];
+        for (var rslt of searchResult) {
+            newPackagesList.push(rslt.item)
+        }
+        renderPackages(newPackagesList, searchString=query)
+    }
+}
+
+function clearPackages() {
+    var mainDiv = document.getElementsByClassName("package-results")[0]
+    while (mainDiv.firstChild) {
+        mainDiv.removeChild(mainDiv.firstChild)
     }
 }
