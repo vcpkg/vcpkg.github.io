b877022a0df958c715b56339840770b055265a38
yaoleo34 <19271429+yaoleo34@users.noreply.github.com>
2020-06-12 14:23:56 -0700

    added scripts to obtain uhf

---
+++ uhf_scripts.py
@@ -0,0 +1,31 @@
+import requests
+import untangle
+
+service_endpoint = "http://uhf.microsoft.com"
+partner_id = "VCPKG"
+header_id = "VCPKGHeader-real"
+footer_id = "VCPKGFooter"
+user_agent = "Microsoft"
+locale = "en-US"
+service_url = service_endpoint + "/" + locale + "/shell/xml/" + partner_id + "?headerId=" + header_id + "&footerId=" + footer_id
+
+r = requests.get(service_url, headers={'user-agent': user_agent})
+xml = untangle.parse(r.text)
+css = xml.shell.cssIncludes.cdata
+js = xml.shell.javascriptIncludes.cdata
+header = xml.shell.headerHtml.cdata
+footer = xml.shell.footerHtml.cdata
+#print()
+output = open("css.html", mode = 'w')
+output.write(str(css))
+output.close()
+output = open("js.html", mode = 'w')
+output.write(str(js))
+output.close()
+output = open("header.html", mode = 'w')
+output.write(str(header))
+output.close()
+output = open("footer.html", mode = 'w')
+output.write(str(footer))
+output.close()
+#print(r.text)
