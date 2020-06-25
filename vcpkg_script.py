55078a866f234fa05f8520b1ee30f00a295806e9
yaoleo34 <t-yunyao@microsoft.com>
2020-06-25 09:30:59 -0700

    oopsies

--- vcpkg_script.py
+++ vcpkg_script.py
@@ -10,17 +10,19 @@ from datetime import datetime
 # modidy this to be your own token or environmental variable for github repo api access
 token = ""
 
+
 def getFiles(path):
     files = os.listdir(path)
     return list(filter(lambda x: x[0] != '.', files))
+
 
 path = "ports"
 data = {}
 data["Generated On"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 files = getFiles(path)
-data["size"] = len(files)
+data["Size"] = len(files)
 jsonlist = []
-#systems found in ci.baseline.txt
+# systems found in ci.baseline.txt
 systems = ["arm64-windows", "arm-uwp", "x64-linux", "x64-osx", "x64-uwp", "x64-windows", "x64-windows-static", "x86-windows"]
 
 
@@ -51,7 +53,7 @@ def gen_all_files():
         jsonf = {}
         jsonf["Name"] = filename
         for x in f:
-            if x[0] == "#":
+            if x.strip() == "" or x.strip()[0] == "#":
                 continue
             idx = x.find(":") # colon delimiter for properties
             if idx == -1 and prev != "" and x[idx+1]==" ":
@@ -74,13 +76,13 @@ jsonlist.sort(key=lambda item: item["Name"])
 
 length = len("github.com")
 base = "https://api.github.com/repos"
-data["source"] = jsonlist
+data["Source"] = jsonlist
 
 
 def get_stars(data):
     count = 1
-    for port in data["source"]:
-        print("Loading " + str(count) + " out of " + str(data["size"]))
+    for port in data["Source"]:
+        print("Loading " + str(count) + " out of " + str(data["Size"]))
         count += 1
         if "Homepage" in port and port["Homepage"].find("github.com") >= 0:
             url = base+port["Homepage"][port["Homepage"].find("github.com")+length :]
@@ -88,9 +90,31 @@ def get_stars(data):
             github_json = json.loads(github)
             if "stargazers_count" in github_json:
                 stars = github_json["stargazers_count"]
-                port["stars"] = stars
+                port["Stars"] = stars
 
 get_stars(data)
+
+def index_of(lst, name):
+    for i in range(len(lst)):
+        if lst[i]["Name"] == name:
+            return i
+    return -1
+
+def get_all_files(data):
+    jsonlist = data["Source"]
+    file = open('VCPKGHeadersDatabase.txt')
+    for line in file:
+        idx = line.strip().find(":")
+        package = line.strip()[:idx]
+        file_name = line.strip()[idx+1:]
+        pos = index_of(jsonlist, package)
+        if "Files" in jsonlist[pos]:
+            jsonlist[pos]["Files"].append(file_name)
+        else:
+            jsonlist[pos]["Files"] = [file_name]
+    file.close()
+
+get_all_files(data)
 out = json.dumps(data, sort_keys=True, indent=4)
 output = open("output.json", mode = 'w')
 output.write(out)
