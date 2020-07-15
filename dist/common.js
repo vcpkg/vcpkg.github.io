16756fb170480b85b9716bde631a80835d3907ff
Patricia Azevedo <t-paazev@microsoft.com>
2020-07-15 09:24:54 -0700

    converting-files

--- dist/common.js
+++ dist/common.js
@@ -1,20 +1,19 @@
 function copyCodePanel(id) {
-    let temp: any = document.getElementById(id);
+    var temp = document.getElementById(id);
     temp.value = temp.textContent;
     temp.select();
     document.execCommand('copy');
     clearSelection();
 }
-
 // remove the highlight from selected text
 function clearSelection() {
     if (window.getSelection) {
         window.getSelection().removeAllRanges();
-    } else if  (document.selection) {
+    }
+    else if (document.selection) {
         document.selection.empty();
     }
 }
-
 // determine what OS the user is on, used to render corresponding package installation code
 // default to Unix unless the user is on a Windows device
 function detectOS() {
---
+++ dist/gettingStarted.js
@@ -0,0 +1,38 @@
+var os = detectOS();
+$(document).ready(function () {
+    clickGetStartedTab(os); //initialized to user's current platform
+    $('.gs-copy-btn').click(function () {
+        var step = $(this).attr('id');
+        if (step === 'step5') {
+            // the code for windows and unix is shared in this case
+            copyCodePanel('all-' + step);
+        }
+        else if (os === 'windows')
+            copyCodePanel('windows-' + step);
+        else
+            copyCodePanel('unix-' + step);
+    });
+    $('.gs-tab-btn').click(function () {
+        var id = $(this).attr('id');
+        clickGetStartedTab(id.substring(7));
+    });
+});
+function clickGetStartedTab(platform) {
+    os = platform;
+    var elems = document.getElementsByClassName(os);
+    var e;
+    $('.show').removeClass('show');
+    $('.selected').removeClass('selected');
+    document.getElementById('gs-btn-' + os).classList.add('selected');
+    for (var _i = 0, elems_1 = elems; _i < elems_1.length; _i++) {
+        e = elems_1[_i];
+        e.classList.add('show');
+    }
+    if (os === 'linux' || os === 'mac') {
+        elems = document.getElementsByClassName('unix');
+        for (var _a = 0, elems_2 = elems; _a < elems_2.length; _a++) {
+            e = elems_2[_a];
+            e.classList.add('show');
+        }
+    }
+}
