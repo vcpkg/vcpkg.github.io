fb56bfe93fc4476167502970b0202c1d37a9653a
Nicole Mazzuca <mazzucan@outlook.com>
2020-09-01 14:10:09 -0700

    add favicon, modify style, other minor changes
    Minor changes include:
    - Add "prepare" script which calls tsc
    - Add "start" script which runs a server
    - Add <head> and <body> to /index.html
    - Change the git repository and bugs url

--- ts/common.ts
+++ ts/common.ts
@@ -5,29 +5,27 @@ let lang = 'en'; //Since there is only one language, set all visitors to english
 // let lang = navigator.language.substring(0, 2); // TODO: use this later to gets the user's actual current language
 
 function copyCodePanel(id) {
-    let temp: HTMLInputElement = document.getElementById(
-        id
-    ) as HTMLInputElement;
-    temp.select();
-    document.execCommand('copy');
-    clearSelection();
+  let temp: HTMLInputElement = document.getElementById(id) as HTMLInputElement;
+  temp.select();
+  document.execCommand('copy');
+  clearSelection();
 }
 
 // remove the highlight from selected text
 function clearSelection() {
-    if (window.getSelection) {
-        window.getSelection().removeAllRanges();
-    }
+  if (window.getSelection) {
+    window.getSelection().removeAllRanges();
+  }
 }
 
 // determine what OS the user is on, used to render corresponding package installation code
 // default to Unix unless the user is on a Windows device
 function detectOS(): Platform {
-    if (/Win/.test(navigator.platform)) {
-        return 'windows';
-    }
-    if (/Mac/.test(navigator.platform)) {
-        return 'mac';
-    }
-    return 'linux';
+  if (/Win/.test(navigator.platform)) {
+    return 'windows';
+  }
+  if (/Mac/.test(navigator.platform)) {
+    return 'mac';
+  }
+  return 'linux';
 }
