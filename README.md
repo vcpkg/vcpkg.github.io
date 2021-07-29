7d6aa088b8da7a25446d9e50b91fc7726917082a
Robert Schumacher <roschuma@microsoft.com>
2021-07-29 10:59:36 -0700

    Add docs generation scripts (#53)
    * add docs
    
    * Complete docs addition. Support Linux.
    
    * Add docker container for rebuilding the website
    
    * Regenerate docs at 8dddc6c899ce6fdbeab38b525a31e7f23cb2d5bb
    
    * Fix broken blog search link
    
    * Regenerate output.json
    
    * Update scripts/Update-PortIndex.ps1
    
    Co-authored-by: dan-shaw <51385773+dan-shaw@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+## vcpkg Website Sources
+
+To regenerate the website, run `scripts/rebuild.sh` from within the docker container defined by `.devcontainer/Dockerfile`.
+
+The commit of https://github.com/Microsoft/vcpkg used is defined within the `rebuild.sh` script.
---
+++ scripts/rebuild.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
+if [ ! -f ../vcpkg ]
+then
+    vcpkg_commit=8dddc6c899ce6fdbeab38b525a31e7f23cb2d5bb
+    git init ../vcpkg
+    git -C ../vcpkg fetch --depth 1 https://github.com/Microsoft/vcpkg $vcpkg_commit
+    git -C ../vcpkg checkout FETCH_HEAD
+fi
+npm install
+pwsh generateDocs.ps1 -destDir .. -sourceDir ../vcpkg/docs
