7f300bdab4a48318bbcfac43428f14575cdd4e7b
Robert Schumacher <roschuma@microsoft.com>
2021-08-04 13:57:26 -0700

    Automatically derive titles from md docs (#54)
    * Automatically derive titles from md docs
    
    * Regenerate with later commit id
    
    Co-authored-by: Robert Schumacher <ras0219@outlook.com>

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
-if [ ! -f ../vcpkg ]
+if [ ! -e ../vcpkg ]
 then
-    vcpkg_commit=8dddc6c899ce6fdbeab38b525a31e7f23cb2d5bb
+    vcpkg_commit=bd5ea16b97e91cb620fed0e10b7d9b3a8a943a52
     git init ../vcpkg
     git -C ../vcpkg fetch --depth 1 https://github.com/Microsoft/vcpkg $vcpkg_commit
     git -C ../vcpkg checkout FETCH_HEAD
