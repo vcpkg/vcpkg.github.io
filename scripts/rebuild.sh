39def2039b00f5343668395f6a37e810d9e8752f
Robert Schumacher <roschuma@microsoft.com>
2022-05-26 20:42:47 +0000

    Update navbar template and vcpkg repo commit. Add validate workflow.

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,13 +1,17 @@
 #/bin/bash
 
+set -e
+
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
 if [ ! -e ../vcpkg ]
 then
-    vcpkg_commit=440075a9fccf52cab386521967e4f074acd1bd34
+    vcpkg_commit=9ab334404665a5792f3e165a9c8fe41462dc0098
     git init ../vcpkg
     git -C ../vcpkg fetch --depth 1 https://github.com/Microsoft/vcpkg $vcpkg_commit
     git -C ../vcpkg checkout FETCH_HEAD
 fi
-npm install
+npm ci
+rm -rf ../en
 node generatePages.js
 node generateDocs.js ../vcpkg/docs
+node validateLinks.js
