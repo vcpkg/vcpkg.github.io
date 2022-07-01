bb09d0419cbd24d9cabf6905c569cb47ff028569
PhoebeHui <20694052+PhoebeHui@users.noreply.github.com>
2022-07-01 02:05:45 -0700

    Update rebuild.sh

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -5,7 +5,7 @@ set -e
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
 if [ ! -e ../vcpkg ]
 then
-    vcpkg_commit=9ab334404665a5792f3e165a9c8fe41462dc0098
+    vcpkg_commit=881c1b04a40f258b23880f9ef3ba89b6ed5c8ecf
     git init ../vcpkg
     git -C ../vcpkg fetch --depth 1 https://github.com/Microsoft/vcpkg $vcpkg_commit
     git -C ../vcpkg checkout FETCH_HEAD
