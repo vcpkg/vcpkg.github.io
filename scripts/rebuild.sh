9f3f6b33b4a48a438e592516ab3de8afb82ea045
Sreenivas-Kalluru <69580789+Sreenivas-Kalluru@users.noreply.github.com>
2023-03-29 17:42:35 +0530

    rebuild checked-in

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -5,17 +5,19 @@ set -e
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
 if [ ! -e ../vcpkg ]
 then
-    vcpkg_commit=57d3194e702a2959e86a6748999ad71fc24f7922
+    vcpkg_commit=1271068e139c1fc30bae405c0bca0e379e155bd2
     git init ../vcpkg
     git -C ../vcpkg fetch --depth 1 https://github.com/Microsoft/vcpkg $vcpkg_commit
     git -C ../vcpkg checkout FETCH_HEAD
 fi
 
+githubToken=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
 npm ci
 rm -rf ../en
 node generatePages.js
 node generateDocs.js
 node validateLinks.js
-node generateGitHubStars.js ../vcpkg $1
+node generateGitHubStars.js ../vcpkg $1 $githubToken
 node generatePackages.js ../vcpkg
 
+read -p "Press enter to continue"
