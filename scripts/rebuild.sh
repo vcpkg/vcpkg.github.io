5a09eb8469fd46a1ff5eec4bbd8574523b15c456
Sreenivasulu Kalluru <69580789+Sreenivas-Kalluru@users.noreply.github.com>
2023-03-30 21:09:55 +0530

    sync package metadata -manual process 2023-03-30 (#107)
    * sync package -manual process
    
    * updated stars information

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -5,7 +5,7 @@ set -e
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
 if [ ! -e ../vcpkg ]
 then
-    vcpkg_commit=57d3194e702a2959e86a6748999ad71fc24f7922
+    vcpkg_commit=d10d511f25620ca0f315cd83dcef6485efc63010
     git init ../vcpkg
     git -C ../vcpkg fetch --depth 1 https://github.com/Microsoft/vcpkg $vcpkg_commit
     git -C ../vcpkg checkout FETCH_HEAD
