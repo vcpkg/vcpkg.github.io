f4848d2dfb8624900810f688f648321532e33f9f
Sreenivas-Kalluru <69580789+Sreenivas-Kalluru@users.noreply.github.com>
2023-05-17 07:11:07 +0530

    validate yml

--- scripts/syncMetadata.sh
+++ scripts/syncMetadata.sh
@@ -3,23 +3,23 @@
 set -e
 
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
-repo_url=https://github.com/Microsoft/vcpkg
-vcpkg_commit=$(git ls-remote $repo_url | head -n1 | awk '{ print $1 }')
-filename="commit.txt"
-if [ ! -f "$filename" ]
-then
-    # create file with default value
-    echo "0" > "$filename"
-    echo "File created with default value: $filename"
-fi
+# repo_url=https://github.com/Microsoft/vcpkg
+# vcpkg_commit=$(git ls-remote $repo_url | head -n1 | awk '{ print $1 }')
+# filename="commit.txt"
+# if [ ! -f "$filename" ]
+# then
+#     # create file with default value
+#     echo "0" > "$filename"
+#     echo "File created with default value: $filename"
+# fi
 
-content=$(cat commit.txt)
+# content=$(cat commit.txt)
 
-if [ $vcpkg_commit != $content ]
-then
-    git init ../vcpkg
-    git -C ../vcpkg fetch --depth 1 https://github.com/Microsoft/vcpkg $vcpkg_commit
-    git -C ../vcpkg checkout FETCH_HEAD
-    echo $vcpkg_commit > commit.txt
-fi
+# if [ $vcpkg_commit != $content ]
+# then
+#     git init ../vcpkg
+#     git -C ../vcpkg fetch --depth 1 https://github.com/Microsoft/vcpkg $vcpkg_commit
+#     git -C ../vcpkg checkout FETCH_HEAD
+#     echo $vcpkg_commit > commit.txt
+# fi
 
