3c29d8a77428fed679cffb117eef918b3371e687
Divyanshu Jain <divyanshu.jain@neudesic.com>
2025-04-04 21:00:34 +0530

    Add cookieOptOut script

---
+++ scripts/cookieOptOut.js
@@ -0,0 +1,26 @@
+(function () {
+    const PRIVACY_ORIGIN = "https://dotnet.microsoft.com";
+    const iFrame = document.createElement("iframe");
+    iFrame.style.width = "0";
+    iFrame.style.height = "0";
+    iFrame.style.position = "absolute";
+    iFrame.style.border = "none";
+    iFrame.src = PRIVACY_ORIGIN + "/dotnetprivacy";
+    let messageEventListener;
+
+    window.optOutPromise = new Promise((resolve, reject) => {
+        messageEventListener = (event) => {
+            if (event.origin === PRIVACY_ORIGIN) {
+                const optOutEventData = event.data?.["3PAdsOptOut"];
+                if (typeof optOutEventData !== "undefined") {
+                    resolve(optOutEventData === "1");
+                }
+                resolve(false);
+                removeEventListener("message", messageEventListener);
+                iFrame.remove();
+            }
+        }
+    });
+    addEventListener("message", messageEventListener);
+    document.body.appendChild(iFrame);
+})();
