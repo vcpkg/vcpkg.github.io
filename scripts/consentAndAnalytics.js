97af2fd0cfdd26f996da81efb114fe5ec42e74d3
Divyanshu Jain <divyanshu.jain@neudesic.com>
2025-04-09 05:06:50 +0530

    Update GPC_DataSharingOptIn value

--- scripts/consentAndAnalytics.js
+++ scripts/consentAndAnalytics.js
@@ -80,21 +80,5 @@ function manageConsent() {
 const globalPrivacyControlEnabled = navigator.globalPrivacyControl;
 
 // set data sharing opt-in to false when GPC/AMC controls detected
-const GPC_DataSharingOptIn = (globalPrivacyControlEnabled) ? false : checkThirdPartyAdsOptOutCookie();
+const GPC_DataSharingOptIn = (globalPrivacyControlEnabled) ? false : await window.optOutPromise;
 analytics.getPropertyManager().getPropertiesContext().web.gpcDataSharingOptIn = GPC_DataSharingOptIn;
-//Detect AMC opt out choice
-function checkThirdPartyAdsOptOutCookie() {
-    try {
-        const ThirdPartyAdsOptOutCookieName = '3PAdsOptOut';
-        var cookieValue = getCookie(ThirdPartyAdsOptOutCookieName);
-        // for unauthenticated users
-        return cookieValue !== 1;
-    } catch {
-        return true;
-    }
-}
-
-function getCookie(cookieName) {
-    var cookieValue = document.cookie.match('(^|;)\\s' + cookieName + '\\s*=\\s*([^;]+)');
-    return (cookieValue) ? cookieValue[2] : '';
-}
