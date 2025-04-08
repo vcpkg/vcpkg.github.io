d700eb50cab2a23f7c353e92bd24a093ff5f2d6a
Divyanshu Jain <divyanshu.jain@neudesic.com>
2025-04-09 05:05:03 +0530

    Update iFrame source location

--- scripts/cookieOptOut.js
+++ scripts/cookieOptOut.js
@@ -5,7 +5,7 @@
     iFrame.style.height = "0";
     iFrame.style.position = "absolute";
     iFrame.style.border = "none";
-    iFrame.src = PRIVACY_ORIGIN + "/dotnetprivacy.html";
+    iFrame.src = PRIVACY_ORIGIN + "/dotnetprivacy";
     let messageEventListener;
 
     window.optOutPromise = new Promise((resolve, reject) => {
