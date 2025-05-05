effa5c0e6d04cbedbd0176182f6fd7154e646f00
Divyanshu Jain <divyanshu.jain@neudesic.com>
2025-05-05 21:04:40 +0530

    Fix async errors

--- scripts/consentAndAnalytics.js
+++ scripts/consentAndAnalytics.js
@@ -80,5 +80,7 @@ function manageConsent() {
 const globalPrivacyControlEnabled = navigator.globalPrivacyControl;
 
 // Set data sharing opt-in to false when GPC/3PAds controls detected
-const GPC_DataSharingOptIn = (globalPrivacyControlEnabled) ? false : await window.optOutPromise;
-analytics.getPropertyManager().getPropertiesContext().web.gpcDataSharingOptIn = GPC_DataSharingOptIn;
+(async function() {
+    const GPC_DataSharingOptIn = (globalPrivacyControlEnabled) ? false : await window.optOutPromise;
+    analytics.getPropertyManager().getPropertiesContext().web.gpcDataSharingOptIn = GPC_DataSharingOptIn;
+})();
