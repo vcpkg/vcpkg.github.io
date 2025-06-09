034b00ba270b34e592534a8eb1ae9a5a4d7e08ad
Billy Robert O'Neal III <bion@microsoft.com>
2025-06-09 12:26:42 -0700

    The Microsoft github org is canonically lowercase.

--- scripts/syncMetadata.sh
+++ scripts/syncMetadata.sh
@@ -3,6 +3,6 @@
 set -e
 
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
-repo_url=https://github.com/Microsoft/vcpkg
+repo_url=https://github.com/microsoft/vcpkg
 (git ls-remote $repo_url | head -n1 | awk '{ print $1 }') > commit.txt
 
--- scripts/updateAndRebuild.sh
+++ scripts/updateAndRebuild.sh
@@ -3,5 +3,5 @@
 set -e
 
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")"
-git ls-remote https://github.com/Microsoft/vcpkg master | sed -nE 's/^([0-9a-f]+)\t[^\n]*$/\1/p' > commit.txt
+git ls-remote https://github.com/microsoft/vcpkg master | sed -nE 's/^([0-9a-f]+)\t[^\n]*$/\1/p' > commit.txt
 ./rebuild.sh
